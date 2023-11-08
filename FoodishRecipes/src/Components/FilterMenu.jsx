69573e9e4b9b37c06177d73e47a1df27268f9b09
Darshan Pandya <andyadarshan811@gmail.com>
2023-11-08 10:42:23 +0530

    [Refactor] FilterMenu

--- FoodishRecipes/src/Components/FilterMenu.jsx
+++ FoodishRecipes/src/Components/FilterMenu.jsx
@@ -5,22 +5,26 @@ const FilterMenu = ({handleFilter}) => {
   
 
   const [selectedValue, setSelectedValue] = useState('');
+  const mealTypeArray =  
+  [{value : '' , label : 'All'},
+  {value : 'breakfast' , label : 'Breakfast'},
+  {value : 'brunch' , label : 'Brunch'},
+  {value : 'lunch/dinner' , label : 'Lunch / Dinner'},
+  {value : 'teatime' , label : 'Teatime'}
+]
 
   const handleDropdownChange = (event) => {
     handleFilter(event.target.value)
     setSelectedValue(event.target.value)
   };
 
-
   return (
     <>
       <div className="filter-menu  flex items-center justify-center w-fit mx-auto flex-wrap">
-        <button className='filter-btn' onClick={handleDropdownChange} value=''>All</button>
-        <button className='filter-btn' onClick={handleDropdownChange} value='breakfast'>Breakfast</button>
-        <button className='filter-btn' onClick={handleDropdownChange} value='brunch'>Brunch</button>
-        <button className='filter-btn' onClick={handleDropdownChange} value='lunch/dinner'>Lunch / Dinner</button>
-        <button className='filter-btn' onClick={handleDropdownChange} value='teatime'>Teatime</button>
-
+        {mealTypeArray.map((mealType) => {
+          const {value , label} = mealType
+          return <button className='filter-btn' onClick={handleDropdownChange} value={value}>{label}</button>
+        })}
       </div>
     </>
   );
